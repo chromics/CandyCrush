b79ac46e2a99629a9c1d917b8c7660706a04df89
Nplus4 <kevin.evan.ko.0523@gmail.com>
2023-12-18 16:20:50 +0700

    Readjust Packages Arrangment again

--- src/Mechanism/CheckMatch.java
+++ src/Mechanism/CheckMatch.java
@@ -1,10 +1,19 @@
 package mechanism;
 
 import model.Cell;
+import data.constant.Constant;
 
 public class CheckMatch {
     // public static 
     public static void findMatches(Cell[][] grid){
 
+        //Loop from Bottom Left of the Board to Top Right
+        for(int row = Constant.BOARD_ROW_SIZE.getNum() - 1; row >= 0; row--){
+            for(int col = 0; col < Constant.BOARD_COL_SIZE.getNum(); col++){
+                if(grid[row][col].containPiece()){
+                    
+                }
+            }
+        }
     }
 }
